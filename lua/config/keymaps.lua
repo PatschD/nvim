8972f2de1bf0ede92ded4d4131797bd7064a1dae
PatschD <david.patsch@gmx.at>
2024-04-19 09:56:24 +0200

    toggleterm

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -14,3 +14,11 @@ vim.keymap.set("n", "<S-h>", ":bprevious<CR>", opts)
 
 -- vim.keymap.set("n", "<Space>lg", "<cmd>lua require('zippy').insert_print()<CR>")
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+
+-- floating terminal
+local lazyterm = function()
+  LazyVim.terminal(nil, { cwd = LazyVim.root() })
+end
+
+vim.keymap.set("n", "<C-t>", lazyterm, { desc = "Terminal (Root Dir)" })
+vim.keymap.set("t", "<C-t>", "<cmd>close<cr>", { desc = "Hide Terminal" })
